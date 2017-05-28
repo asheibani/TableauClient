7ee2d62195672f90d85dc197a047cb173a817837
Amir Sheibani <amir.sheibani@ventivtech.com>
2017-05-28 02:22:59 -0400

    add logging

--- README.md
+++ README.md
@@ -12,13 +12,14 @@ In the application.properties, you must define:
 3. tableau.password: The user's password
 4. tableau.siteId: The Tableau site id
 5. tableau.workbookId: The Tableau workbook id that contains the views
+6. tableau.logLevel: select NONE, BASIC, HEADERS, or FULL
 
 Once you have added all the properies, you can start the application.
 
 To view the views of a given workbook:
-GET http://localhost:8080/tableau-client/views
+`GET http://localhost:8080/tableau-client/views`
 
 To view a particular report, find a contentUrl in the response workbook.views.view.contentUrl.
 
 To load a view:
-GET http://localhost:8080/tableau-client/html?contentUrl=[the contentUrl]
+`GET http://localhost:8080/tableau-client/html?contentUrl=[the contentUrl]`
--- src/main/groovy/org/sheibani/tableauclient/config/TableauConfig.groovy
+++ src/main/groovy/org/sheibani/tableauclient/config/TableauConfig.groovy
@@ -2,15 +2,18 @@ package org.sheibani.tableauclient.config
 
 import feign.Feign
 import feign.Logger
+import feign.Logger.Level
 import feign.gson.GsonDecoder
 import feign.jackson.JacksonEncoder
 import feign.slf4j.Slf4jLogger
+import groovy.util.logging.Slf4j
 import org.sheibani.tableauclient.client.TableauApiClient
 import org.sheibani.tableauclient.client.TableauClient
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+import javax.annotation.PostConstruct
 import java.nio.file.attribute.GroupPrincipal
 import java.nio.file.attribute.UserPrincipal
 import java.nio.file.attribute.UserPrincipalLookupService
@@ -18,6 +21,7 @@ import java.nio.file.attribute.UserPrincipalLookupService
 /**
  * Created by asheibani on 5/28/17.
  */
+@Slf4j
 @Configuration
 @ConfigurationProperties(prefix="tableau")
 class TableauConfig {
@@ -29,13 +33,25 @@ class TableauConfig {
     String workbookId
     String apiVersion
     String contentUrlFilter
+    String logLevel
+
+    Level level
+
+    @PostConstruct
+    public void init() {
+        try {
+            level = Level.valueOf(logLevel)
+        } catch(Throwable t) {
+            log.warn("Invalid log level ${logLevel}, setting to FULL")
+        }
+    }
 
     @Bean
     public TableauApiClient tableauApiClient() {
         TableauApiClient client =
                 Feign.builder()
                         .logger(new Slf4jLogger())
-                        .logLevel(Logger.Level.FULL)
+                        .logLevel(level)
                         .encoder(new JacksonEncoder())
                         .decoder(new GsonDecoder())
                         .target(TableauApiClient.class, url + apiVersion)
@@ -48,7 +64,7 @@ class TableauConfig {
         TableauClient client =
                 Feign.builder()
                         .logger(new Slf4jLogger())
-                        .logLevel(Logger.Level.FULL)
+                        .logLevel(level)
                         .target(TableauClient.class, url)
 
         return client
